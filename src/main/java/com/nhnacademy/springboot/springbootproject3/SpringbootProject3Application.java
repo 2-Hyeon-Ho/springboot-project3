a468ad258fd22688fc00cc244384dfa90c838ab5
ihyeonho <dpqlvkvk@naver.com>
2022-12-21 16:43:41 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/springboot/springbootproject3/SpringbootProject3Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.springboot.springbootproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject3Application.class, args);
+	}
+
+}
